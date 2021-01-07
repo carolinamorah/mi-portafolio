a2f49fef48a165c1009606a2146d4ed614e38786
Carolina Mora <carolinamorahernandez@hotmail.com>
2021-01-06 22:56:22 -0300

    Added new code to index.html for generate popovers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,14 @@
 $(function(){
   $("a").on("click", function (e) {
-  // 1
   e.preventDefault();
-  // 2
+
   const href = $(this).attr("href");
-  // 3
+
   $("html, body").animate({ scrollTop: $(href).offset().top }, 800);
 });
+
+$('[data-toggle="popover"]').popover()
+
+
+
 });
