3d8c1e93d945521f3810f30b0fa0f1c67e6c2ff3
Carolina Mora <carolinamorahernandez@hotmail.com>
2021-01-06 22:21:54 -0300

    added animation effects to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(function(){
+  $("a").on("click", function (e) {
+  // 1
+  e.preventDefault();
+  // 2
+  const href = $(this).attr("href");
+  // 3
+  $("html, body").animate({ scrollTop: $(href).offset().top }, 800);
+});
+});
